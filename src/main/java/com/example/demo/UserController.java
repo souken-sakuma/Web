e8be35bc0046cf6a762948f5272826fb7c8c12b7
souken-sakuma <m-sakuma@souken-sys.co.jp>
2026-05-29 17:04:11 +0900

    Entitiyの作成

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	private final UserRepository repo;
+	
+	public UserController(UserRepository repo) {
+		this.repo = repo;
+	}
+	
+	@PostMapping
+	public User create(@RequestBody User user) {
+		return repo.save(user);
+	}
+	
+	@GetMapping
+	public List<User>findAll(){
+		return repo.findAll(); 
+	}
+
+}
